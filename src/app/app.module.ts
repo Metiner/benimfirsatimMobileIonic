b1af8799f8dc4170f61bab4a2061f5e615580d98
Gokay Kucuk <serdargokaykucuk@gmail.com>
2017-10-30 22:10:19 +0100

    Veri gosteremiyor ama cekebiliyor.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {HttpModule} from "@angular/http";
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {BenimfirsatimLib} from "./benimfirsatimLib";
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,6 +27,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    BenimfirsatimLib,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Loading,NavController } from 'ionic-angular';
+import {BenimfirsatimLib} from "../../app/benimfirsatimLib";
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,14 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  hotDeals = null;
 
+  constructor(public navCtrl: NavController,benimfirsatimLib:BenimfirsatimLib) {
+    benimfirsatimLib.getPage('hot').subscribe((data)=>{
+      this.hotDeals = data;
+    })
   }
+
+
 
 }
